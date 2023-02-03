b8211ff7ff6fb3a645f3f6f678116cf4b215625e
Quiholder <119394603+Quiholder@users.noreply.github.com>
2023-02-02 20:33:13 -0500

    added js file

---
+++ javascript_file.js
@@ -0,0 +1,8 @@
+document.write("Lisa told Bart, \"knock it off, Bart! Or I'll tell dad!")
+document.write("\Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Seuss");
+var B="Concatenated"+ "String"
+document.write(B)
+var Family="The Holders", Dad="John", Mom="Lisa", Daughter="Quiana", Son="Jason";
+document.write(Son);
