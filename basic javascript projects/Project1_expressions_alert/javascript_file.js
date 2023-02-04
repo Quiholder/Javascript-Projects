3a91609c27e30301b87112498c210fe7205ed417
Quiholder <119394603+Quiholder@users.noreply.github.com>
2023-02-03 22:01:27 -0500

    javascript project

---
+++ basic javascript projects/Project1_expressions_alert/javascript_file.js
@@ -0,0 +1,19 @@
+window.alert('Hello,World');
+document.write("Lisa told Bart, \"knock it off, Bart! Or I'll tell dad!")
+document.write("\Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Seuss");
+var B="Concatenated"+ "String"
+document.write(B)
+var Family="The Holders", Dad="John", Mom="Lisa", Daughter="Quiana", Son="Jason";
+document.write(Son+Mom);
+//the sum 6 is displayed with the below doc//
+document.write(3+3);
+//this function display a click me button once clicked displays this is the button text//
+function My_First_Function() {
+    var str= "this is the button text!";//defining a variable and giving it a string value//
+    document.getElementById ("Button_Text").innerHTML=str;//putting the value of the variable into the HTML elementFromPoint with the "Button_Text" id
+}
+function displayDate() {
+    var str="thie time is?";
+}
---
+++ basic javascript projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function New_Order() {
+    var str="place order now";
+    document.getElementById ("Button_Text").innerHTML=str;
+}
+function myFunction () {
+    var sentence="Iam learning";
+    sentence +="a lot from this book";
+    document.getElementById("Concatenate").innerHTML=sentence;
+    }
