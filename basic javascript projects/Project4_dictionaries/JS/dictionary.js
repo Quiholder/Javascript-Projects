d5d1265b1d932f697a3b9837dda2d47eccf168b6
Quiholder <119394603+Quiholder@users.noreply.github.com>
2023-02-08 18:20:41 -0500

    for review

---
+++ basic javascript projects/Project4_dictionaries/JS/dictionary.js
@@ -0,0 +1,10 @@
+function myDictionary() {
+   var Animal= {
+    Species: "Dog",
+    Color: "Black",
+    Breed: "Bulldog",
+    Age: 2,
+    Sound: "Bark!"
+};
+document.getElementById("Dictionary").innerHTML=Animal.Sound;
+}
